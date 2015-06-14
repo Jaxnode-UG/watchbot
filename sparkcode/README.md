740fe4b1437a498e85fb02ab5c78ba3f011af416
David Fekke <david@fekke.com>
2015-06-14 17:05:13 -0400

    Adding revised Sumocar code

---
+++ sparkcode/README.md
@@ -0,0 +1,3 @@
+# Code for Particle Spark Core or Particle Photon
+
+Load this code onto your sumobot to accept commands from the Pebble Watch
--- sparkcode/sumocar.ino
+++ sparkcode/sumocar.ino
@@ -8,11 +8,9 @@ Servo rightServo;
 void setup()
 {
   //Register Spark function
-  //Spark.function("sumocar", rcCarControl);
   Spark.function("sumocar", setMotorpower);
   leftServo.attach(D0);
   rightServo.attach(D1);
-  //pinMode(D7,OUTPUT);
 }
 
 /* This function loops forever --------------------------------------------*/
@@ -22,61 +20,15 @@ void loop()
 }
 
 /*******************************************************************************
- * Function Name  : rcCarControl
+ * Function Name  : setMotorpower
  * Description    : Parses the incoming API commands and sets the motor control
                     pins accordingly
  * Input          : RC Car commands
-                    e.g.: rc,FORWARD
-                          rc,BACK
+                    e.g.: args=0,180
+                          args=90,135
  * Output         : Motor signals
  * Return         : 1 on success and -1 on fail
  *******************************************************************************/
-int rcCarControl(String command)
-{
-  if(command.substring(0,4) == "STOP")
-  {
-
-    leftServo.write(90);
-    rightServo.write(90);
-
-    return 1;
-  }
-
-  if(command.substring(0,4) == "BACK")
-  {
-    leftServo.write(0);
-    rightServo.write(180);
-    
-    return 1;
-  }
-
-  if(command.substring(0,7) == "FORWARD")
-  {
-    leftServo.write(180);
-    rightServo.write(0);
-
-    return 1;
-  }
-
-  if(command.substring(0,5) == "RIGHT")
-  {
-    leftServo.write(180);
-    rightServo.write(180);
-
-    return 1;
-  }
-
-  if(command.substring(0,4) == "LEFT")
-  {
-    leftServo.write(0);
-    rightServo.write(0);
-    return 1;
-  }
-
-  // If none of the commands were executed, return false
-  return -1;
-}
-
 int setMotorpower(String command) {
 
   int loc1 = 0;
